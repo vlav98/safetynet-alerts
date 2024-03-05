c028e64234797cdfec116524e7550063f1aa5125
Valentine Lav <valentinelav98@gmail.com>
2024-03-05 16:35:53 +0100

    :tada: Initialize SafetyNet Alerts application

---
+++ src/main/java/com/oc/safetynetalerts/SafetynetAlertsApplication.java
@@ -0,0 +1,13 @@
+package com.oc.safetynetalerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetynetAlertsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafetynetAlertsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oc/safetynetalerts/SafetynetAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oc.safetynetalerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetynetAlertsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
